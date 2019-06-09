3947443953bcc763f8735070ec3346a815ecfe5c
robq97 <39276231+robq97@users.noreply.github.com>
2019-06-09 13:28:06 -0600

    Se creó modelo de ContactClient y Task

---
+++ Bostream/Bostream/Models/ContactClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bostream.Models
+{
+    public class ContactClient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Notes { get; set; }
+        public Tasks Tasks { get; set; }
+    }
+}
