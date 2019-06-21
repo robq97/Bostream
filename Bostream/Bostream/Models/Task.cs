2e3cc9cefa38ccb1f62d61adbc13b053545b7306
mseguraa600 <47221828+mseguraa600@users.noreply.github.com>
2019-06-21 14:49:52 -0600

    Tasks

--- Bostream/Bostream/Models/Task.cs
+++ Bostream/Bostream/Models/Task.cs
@@ -7,5 +7,11 @@ namespace Bostream.Models
 {
     public class Task
     {
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public DateTime LimitDate { get; set; }
+        public string Desc { get; set; }
+        public int ImporLev { get; set; }
+       
     }
 }
