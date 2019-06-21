10df49fa8c7f761a6c322700b4c751d25879f66f
apereiraaguirre <a.pereiraaguirre@gmail.com>
2019-06-21 16:22:19 -0600

    Mayúscula de atributos AdminUser

--- Bostream/Bostream/Models/AdminUser.cs
+++ Bostream/Bostream/Models/AdminUser.cs
@@ -3,9 +3,9 @@ namespace Bostream.Models
 {
     public class AdminUser
     {
-        public int id { get; set; }
-        public string nombre { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
