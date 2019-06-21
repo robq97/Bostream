70adfbc4d29179fd7e1495d09618011b6f74f024
Andrés Pereira <apereira@mun.ca>
2019-06-21 16:11:30 -0600

    Se implementa modelo de AdminUser

---
+++ Bostream/Bostream/Models/AdminUser.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Bostream
+{
+    public class AdminUser
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
