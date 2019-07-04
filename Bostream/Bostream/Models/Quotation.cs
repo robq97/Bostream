6d9d2b335a52cff63f0be4f9c7a93b1efc7d0ab6
robq97 <39276231+robq97@users.noreply.github.com>
2019-07-04 14:44:04 -0600

    Se crearon modelos de Service, Quotation, y Newsletter

--- Bostream/Bostream/Models/Quotation.cs
+++ Bostream/Bostream/Models/Quotation.cs
@@ -5,13 +5,12 @@ using System.Web;
 
 namespace Bostream.Models
 {
-    public class Task
+    public class Quotation
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public DateTime DeadLine { get; set; }
-        public string Description { get; set; }
-        public int Priority { get; set; }
         public int CustomerId { get; set; }
+        public int ServiceId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
     }
 }
---
+++ Bostream/Bostream/Models/Service.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bostream.Models
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string ServiceName { get; set; }
+    }
+}
