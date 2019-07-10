733449b66c249493eebd6356cd1956f7531e6d3d
amonteroa <amonteroa548@ulacit.ed.cr>
2019-07-10 10:42:35 -0600

    enlazar base de datos a la seccion de damin

--- BostreamWeb/BostreamWeb/Controllers/AdminController.cs
+++ BostreamWeb/BostreamWeb/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using BostreamWeb.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.SqlClient;
 
 namespace Bostream.Controllers
 {
@@ -15,6 +17,26 @@ namespace Bostream.Controllers
         public ActionResult LogIn()
         {
             return View();
+        }
+
+        public ActionResult Authentication(BostreamWeb.Models.Admin adminModel)
+        {
+            using (BostreamEntities1 db = new BostreamEntities1())
+            {
+                var adminDetails = db.Admins.Where(x =>
+               x.Username == adminModel.Username &&
+               x.Password == adminModel.Password).FirstOrDefault();
+                if (adminDetails == null)
+                {
+                    adminModel.LoginErrorMessage = "wrong credentials";
+                    return View("LogIn", adminModel);
+                }
+                else
+                {
+                    Session["adminId"] = adminDetails.AdminId;
+                    return RedirectToAction("Index", "QuotationController");
+                }
+            }
         }
     }
 }
---
+++ BostreamWeb/BostreamWeb/Controllers/QuotationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BostreamWeb.Controllers
+{
+    public class QuotationController : Controller
+    {
+        // GET: Quotation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
