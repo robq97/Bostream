1e80cc64a3e90c5668760d7a680040db259d58d1
robq97 <39276231+robq97@users.noreply.github.com>
2019-07-22 16:42:13 -0600

    Se agregó función para enviar email de confirmación cuando se solicita mas info.

--- BostreamWeb/BostreamWeb/Controllers/InformationController.cs
+++ BostreamWeb/BostreamWeb/Controllers/InformationController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,6 +17,48 @@ namespace Bostream.Controllers
         public ActionResult InformationForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendEmail(string name, string lastName, string company, string receiver, string phone
+            ,string service1, string service2, string service3, string service4, string subject, string message)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var senderEmail = new MailAddress("bostreamMarketing@gmail.com", "Bostream Marketing");
+                    var receiverEmail = new MailAddress(receiver, name + " " + lastName);
+                    var password = "bostream2019";
+                    var sub = "Confirmación de Recibido";
+                    var body = "Gracias por contactarnos " + name + ", nos aseguremos de contactarlo por medio de este medio" +
+                        " o bien al número ingresado (" + phone + ") para brindarle más información del servicio en questión. \n \n" +
+                        "Atentamente, Mattia Masanes - Gerente general.";
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(senderEmail.Address, password)
+                    };
+                    using (var mess = new MailMessage(senderEmail, receiverEmail)
+                    {
+                        Subject = sub,
+                        Body = body
+                    })
+                    {
+                        smtp.Send(mess);
+                    }
+                    return RedirectToAction("InformationForm");
+                }
+            }
+            catch (Exception)
+            {
+                ViewBag.Error = "Some Error";
+            }
+            return RedirectToAction("InformationForm");
         }
     }
 }
